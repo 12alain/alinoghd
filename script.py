fa682b2a4f5d79ddf23ec98e98a544e26f346ff9
12alain <kiemdealain02@gmail.com>
2024-08-16 16:10:52 +0000

    update

--- script.py
+++ script.py
@@ -1,10 +1,27 @@
-import requests
 import os
+import requests
+import pandas as pd
 
-def download(url,dataset='data_file.csv'):
-    response=requests.get(url)
+def download(url, dataset='data_file.csv'):
+    # Créez le répertoire 'data/raw' s'il n'existe pas
+    os.makedirs('data/raw', exist_ok=True)
+
+    # Téléchargez le fichier depuis l'URL
+    response = requests.get(url)
+    
+    # Déterminez le chemin du fichier
     file_path = os.path.join('data/raw', dataset)
-    with open(file_path,'wb') as file:
-
+    
+    # Écrivez le contenu du fichier dans le chemin spécifié
+    with open(file_path, 'wb') as file:
         file.write(response.content)
-     
+    
+    print(f"Fichier téléchargé et enregistré à : {file_path}")
+
+    # Chargez le fichier CSV dans un DataFrame
+    df = pd.read_csv(file_path)
+    
+    # Retournez le DataFrame
+    return df
+
+
